52ff5dfd96f763b07c9d7e728f5c16e9134fc883
Name <gigantina.ru@gmail.com>
2020-05-13 01:53:05 +0300

    game

--- bot.py
+++ bot.py
@@ -7,21 +7,45 @@ import functions as f
 TOKEN = '1114362533:AAEBwGiAgdotOuwqWFLXCbmGTf2yCJIENQU'
 
 bot = telebot.TeleBot(TOKEN)
+users = {}
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    global users
+    users[message.from_user.id] = {}
+    users[message.from_user.id]['game'] = False
+
+
     bot.send_message(message.chat.id,
                      "Привет, {}! Я Длиннохвостик - веселый питон, а что самое интересное, я написан на Python,как иронично) Я могу стать отличным собеседником, могу рассказать шутку, мы можем поиграть в камень-ножницы-бумагу, отправить мем или оценить вашу фотографию. А также много чегоеще, я надеюсь, что вам со мной будет интересно)".format(
                          message.from_user.first_name))
 
-
 @bot.message_handler(content_types=['text'])
 def dialog(message):
-    if ("шутк" in str(message.text).lower() or "шутей" in str(
+    global users
+    if ("шутк" in str(message.text).lower() or "шуте" in str(
             message.text).lower() or "прикол" in str(message.text).lower()) and ("прикольно" not in str(message.text).lower()):
         joke = f.jokes()
         bot.send_message(message.chat.id, joke)
+    elif 'камень-ножницы-бумаг' in str(message.text).lower():
+        if users[message.from_user.id]['game']:
+            bot.send_message(message.chat.id, "Мы уже играем! Скорее, выбирай!")
+        users[message.from_user.id]['game'] = True
+        bot.send_message(message.chat.id, "Выбирай!")
+
+    elif users[message.from_user.id]['game'] and message.text.lower() in ["камень", "ножницы", "бумага"]:
+        res = f.game(message.text)
+        bot.send_message(message.chat.id, res + '\n' + "Сыграем еще?)")
+
+    elif users[message.from_user.id]['game'] and message.text.lower() == "да":
+        users[message.from_user.id]['game'] = True
+
+    elif users[message.from_user.id]['game'] and message.text.lower() == "нет":
+        users[message.from_user.id]['game'] = False
+
+    elif users[message.from_user.id]['game']:
+        bot.send_message(message.chat.id, "Ты ввел что-то неправильно, повтори пожалуйста!")
     else:
         bot.send_message(message.chat.id, "a)")
 
--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import random as r
+from emoji import emojize
+
+emoji = {}
+emoji["камень"] = emojize(':package:')
+emoji["ножницы"] = emojize(':scissors:')
+emoji["бумага"] = emojize(':newspaper:')
 
 
 def jokes():
@@ -8,4 +14,18 @@ def jokes():
     joke = lines.split('@')
     r.shuffle(joke)
     return joke[0]
-jokes()
+
+
+def game(choice):
+    global emoji
+    computer = r.choice(["камень", "ножницы", "бумага"])
+    choice = choice.lower()
+
+    if computer == choice:
+        res = "Ничья! Я тоже выбрал " + emoji[computer]
+    elif (computer == "камень" and choice == "бумага") or (computer == "бумага" and choice == "ножницы") or (
+            computer == "ножницы" and choice == "камень"):
+        res = "Вы выиграли! Я выбрал " + emoji[computer]
+    else:
+        res = "Вы проиграли! Я выбрал " + emoji[computer]
+    return res
